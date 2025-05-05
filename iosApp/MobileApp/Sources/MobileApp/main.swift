5ef8f22784e6ee1724beb4f265b3266bc5a200ee
pocchower <mcarrdyson@outlook.com>
2025-05-05 14:53:14 -0400

    Add minimal Swift app and build workflow for IPA

---
+++ iosApp/MobileApp/Sources/MobileApp/main.swift
@@ -0,0 +1,16 @@
+import UIKit
+import MobileAPI
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        print("Dummy app launched.")
+        return true
+    }
+}
+
